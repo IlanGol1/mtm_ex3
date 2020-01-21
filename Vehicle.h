806da95eae4e1b566b703d96505f08d6d307749f
ilan goldenberg <thkiwow@gmail.com>
2020-01-21 13:41:56 +0200

    more changes

--- Vehicle.h
+++ Vehicle.h
@@ -7,6 +7,7 @@
 #include "ParkingLotPrinter.h"
 
 using namespace ParkingLotUtils;
+using std::ostream;
 
 class Vehicle {
 
@@ -15,21 +16,29 @@ class Vehicle {
 	Time entrance;
 	ParkingSpot spot;
 	bool was_fined;
-	
+
 public:
-    bool inspect(Time inspection_time);
-    Vehicle (VehicleType type, LicensePlate plate, Time entrance);
-    Vehicle(const Vehicle& other);
-    VehicleType typeOfVehicle() const;
-    double timeParking(Time exit_time) const;
+	bool inspect(Time inspection_time);
+	Vehicle(VehicleType type, LicensePlate plate, Time entrance);
+	Vehicle(const Vehicle& other);
+	VehicleType typeOfVehicle() const;
+	double timeParking(Time exit_time) const;
 	void setParkingSpot(ParkingSpot& p);
 	ParkingSpot getParkingSpot() const;
 	bool wasFined() const;
-	bool operator==(const Vehicle& vehicleB) const;
-	ostream& operator<<(ostream& os) const;
+	friend bool operator==(const Vehicle& vehicleA, const Vehicle& vehicleB) const;
+	friend ostream& operator<<(ostream& os, const Vehicle v) const;
 
 private:
 	bool fine();
 };
+
+ostream& operator<<(ostream& os, const Vehicle v) const{
+	ParkingLotPrinter::printVehicle(os, v.type, v.plate, v.entrance);
+}
+
+bool operator==(const Vehicle& vehicleA, const Vehicle& vehicleB) const {
+	return vehicleA.plate == vehicleB.plate;
+}
 
 #endif
